f9838a1e069e582d1c474625de6ccfb2ceb3d1b8
Dummk0pf <myothermailid1@gmail.com>
2024-02-26 23:04:31 +0530

    Full text printing

--- src/main/java/my/projects/TextEditor.java
+++ src/main/java/my/projects/TextEditor.java
@@ -1,12 +1,17 @@
 package my.projects;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TextEditor {
 
     private WindowsTerminal wTerminal;
+    private int cursorX;
+    private int cursorY;
     private int rows;
     private int columns;
+    private static List<Integer> keys;
 
     private final static int ARROW_UP = 1000;
     private final static int ARROW_DOWN = 1001;
@@ -17,11 +22,18 @@ public class TextEditor {
     private final static int PAGE_UP = 1006;
     private final static int PAGE_DOWN = 1007;
     private final static int DELETE = 1008;
+    private final static int BACKSPACE = 127;
 
     public TextEditor() {
         wTerminal = WindowsTerminal.getInstance();
         rows = wTerminal.getWindowSize().rows();
         columns = wTerminal.getWindowSize().columns();
+
+        cursorX = 1;
+        cursorY = 1;
+        keys = new ArrayList<>(
+                List.of(ARROW_DOWN, ARROW_UP, ARROW_LEFT, ARROW_RIGHT, HOME, BACKSPACE, DELETE, PAGE_DOWN, PAGE_UP,
+                        END));
     }
 
     public void openNewFile(String path) {
@@ -29,7 +41,7 @@ public class TextEditor {
         startListening();
         while (canRead) {
             try {
-                // refreshScreen();
+                refreshScreen();
                 int key = readKey();
                 canRead = keyAction(key);
 
@@ -44,15 +56,21 @@ public class TextEditor {
 
         StringBuilder screen = new StringBuilder();
         String title = "\033[7mEditor";
-        int pad = columns - title.length();
-        title = title + " ".repeat(pad) + "\033[0m\r\n";
+        String footer = "\033[7mROWS: " + rows + " X: " + cursorX + " Y: " + cursorY;
 
-        screen.append("\033[2J");
+        title = title + " ".repeat(columns - title.length() + 4) + "\033[0m\r\n";
+        footer = footer + " ".repeat(columns - footer.length() + 4) + "\033[0m";
+
+        screen.append("\033[3J");
+        screen.append("\033[H");
         screen.append(title);
         for (int i = 1; i < rows - 1; i++) {
             screen.append("$\r\n");
         }
-        screen.append("\033[2H");
+        screen.append(footer);
+        // This line resets the cursor again at the top
+        // here the cursorX and cursorY are specified to repaint the screen
+        screen.append(String.format("\033[%d;%dH", cursorY + 1, cursorX + 1));
 
         System.out.print(screen.toString());
     }
@@ -114,12 +132,61 @@ public class TextEditor {
     }
 
     private boolean keyAction(int key) {
-        System.out.println((char) key + " -> " + key);
+
+        if (keys.contains(key)) {
+            moveCursor(key);
+        } else {
+            cursorX++;
+            System.out.print((char) key);
+        }
+
         if ((char) key == 'q') {
             stopListening();
             return false;
         }
         return true;
+    }
+
+    private void moveCursor(int key) {
+
+        if (key == ARROW_UP) {
+            if (cursorY > 1) {
+                cursorY--;
+            }
+        }
+
+        else if (key == ARROW_DOWN) {
+            if (cursorY < rows - 1) {
+                cursorY++;
+            }
+        }
+
+        else if (key == ARROW_RIGHT) {
+            if (cursorX < columns - 1) {
+                cursorX++;
+            }
+        }
+
+        else if (key == ARROW_LEFT) {
+            if (cursorX > 1) {
+                cursorX--;
+            }
+        }
+
+        else if (key == HOME) {
+            cursorX = 2;
+        }
+
+        else if (key == END) {
+            cursorX = columns;
+        }
+
+        // Implemented backspace by moving the cursor to the left and overwriting a
+        // space
+        else if (key == BACKSPACE) {
+            System.out.print("\033[D ");
+            cursorX--;
+        }
     }
 
     private void stopListening() {
@@ -128,7 +195,7 @@ public class TextEditor {
     }
 
     private void clearScreen() {
-        System.out.print("\033[2J");
+        System.out.print("\033[3J");
         System.out.print("\033[H");
     }
 }
